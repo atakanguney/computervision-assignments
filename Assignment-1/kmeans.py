523c308382f912f225fe9fa78410c0a3538557bf
atakanguney <atakanguney58@hotmail.com>
2018-10-12 11:05:02 +0300

    add comments
    * add comments to "KMeans" class
    * add some additional comment to third tasks script

--- Assignment-1/kmeans.py
+++ Assignment-1/kmeans.py
@@ -21,6 +21,8 @@ class KMeans:
         self.dim = data.shape[1]
 
         n_samples = data.shape[0]
+
+        # Start with random samples
         labels = np.random.multinomial(1, np.ones(self.n_clusters)/self.n_clusters, n_samples)
 
         # Initialize for stop conditions
@@ -35,25 +37,36 @@ class KMeans:
             self.update_centroids(data, labels)
             labels = self.labels(data)
 
-        # Return centroids
+        # Return trained model
         return self
 
     def should_stop(self, old_centroids, iter_):
+        # Check centroids and old centroids values are assigned
         if (self.centroids is None) or (old_centroids is None):
             return False
         return (iter_ > self.max_iter) or np.allclose(old_centroids.ravel(), self.centroids.ravel())
 
     def labels(self, data):
+        # Calculate distance matrix which is n_samples x n_clusters
         dist_mtx = calculate_dists(data, self.centroids)
         return np.eye(self.n_clusters)[dist_mtx.argmin(axis=1)]
 
     def predict(self, data):
+        # Get labels
         labels = self.labels(data)
+
+        # Get cluster ids from 0 to n_clusters - 1
         return labels.argmax(axis=1)
 
     def update_centroids(self, data, labels):
+        # Formula comes from Matrix Factorization approach for k-Means algorithm
         self.centroids = np.linalg.pinv(labels.T.dot(labels)).dot(labels.T).dot(data)
 
 
 def calculate_dists(X, M):
+    # Note: these are not real distances
+    # X^2 is not calculated, because for each row
+    # same element would be used, and that does not
+    # affect the result of np.argmin(distance_matrix, axis=1)
+    # function
     return -2 * X.dot(M.T) + (M*M).sum(axis=1)[np.newaxis, :]
--- Assignment-1/third.py
+++ Assignment-1/third.py
@@ -16,18 +16,27 @@ def normalize(seq, input_start, input_end, output_start, output_end):
     return (output_start + factor * (seq - input_start)).astype(np.uint8)
 
 
-n_clusters = 2
+n_clusters = 3
 
-while n_clusters < 4:
+while n_clusters < 5:
     for img in imgs:
+        # Prepare data for k-Means algorithm
         cvectors = img.reshape(-1, 3)
+
+        # Create the model
         kmeans = KMeans(n_clusters=n_clusters)
+        # Fit the model
         model = kmeans.fit(cvectors)
+        # Get cluster id for each data point in data set
         pred = model.predict(cvectors)
 
+        # Reshape it as original image
         r_pred = pred.reshape(img.shape[:-1])
+        # Convert dtype to np.uint8
         r_pred = r_pred.astype(np.uint8)
-
+        # To see difference among clusters
+        # Map cluster ids from range(0, n_clusters-1)
+        # to range(0, 255)
         r_pred = normalize(r_pred, 0, n_clusters-1, 0, 255)
 
         plt.imshow(r_pred, cmap='gray')
